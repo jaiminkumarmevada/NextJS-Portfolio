ad0b287546d0ec75f0a48b283307e002760b32e6
Jaimin Mevada <jaiminkumarmevada@gmail.com>
2024-08-10 18:11:21 -0800

    title edited

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>React@Jaimin_mevada</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
